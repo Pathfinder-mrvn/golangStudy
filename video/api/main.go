e4f6be636d87624b3b872e04157e4f35562a610c
Ziyuan Guo <guo.zy@sjtu.edu.cn>
2024-10-27 15:38:34 +0800

    111

---
+++ video/api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func RegisterHandlers() *httprouter.Router {
+	router := httprouter.New()
+	router.POST("/user", CreateUser)
+
+	return router
+}
+func main() {
+	r := RegisterHandlers()
+	err := http.ListenAndServe(":8000", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
